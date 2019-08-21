1ce7eefe2110081345ff100f8d983106e94ee8cf
Lucas Aquiles <lucasaquiles@Lucass-MacBook-Pro.local>
2019-08-20 23:29:49 -0300

    update ignore file

---
+++ product-repository/build/tmp/kapt3/stubs/main/com/lucasaquiles/micronaut/Application.java
@@ -0,0 +1,16 @@
+package com.lucasaquiles.micronaut;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 1, 13}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\u001e\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0010\u0002\n\u0000\n\u0002\u0010\u0011\n\u0002\u0010\u000e\n\u0002\b\u0002\b\u00c6\u0002\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002J\u001b\u0010\u0003\u001a\u00020\u00042\f\u0010\u0005\u001a\b\u0012\u0004\u0012\u00020\u00070\u0006H\u0007\u00a2\u0006\u0002\u0010\b\u00a8\u0006\t"}, d2 = {"Lcom/lucasaquiles/micronaut/Application;", "", "()V", "main", "", "args", "", "", "([Ljava/lang/String;)V", "product-repository"})
+public final class Application {
+    public static final com.lucasaquiles.micronaut.Application INSTANCE = null;
+    
+    public static final void main(@org.jetbrains.annotations.NotNull()
+    java.lang.String[] args) {
+    }
+    
+    private Application() {
+        super();
+    }
+}
