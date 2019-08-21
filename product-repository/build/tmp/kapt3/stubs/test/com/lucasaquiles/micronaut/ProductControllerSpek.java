4658feab71a53598df61b4f27943803e33f4cba8
Lucas Aquiles <lucasaquiles@gmail.com>
2019-08-20 23:29:49 -0300

    update ignore file

---
+++ product-repository/build/tmp/kapt3/stubs/test/com/lucasaquiles/micronaut/ProductControllerSpek.java
@@ -0,0 +1,11 @@
+package com.lucasaquiles.micronaut;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 1, 13}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\f\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002\u00a8\u0006\u0003"}, d2 = {"Lcom/lucasaquiles/micronaut/ProductControllerSpek;", "Lorg/spekframework/spek2/Spek;", "()V", "product-repository"})
+public final class ProductControllerSpek extends org.spekframework.spek2.Spek {
+    
+    public ProductControllerSpek() {
+        super(null);
+    }
+}
