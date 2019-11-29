da9be9d5ab11740e85104093e2334bab971892dd
Lucas Aquiles <lucasaquiles@gmail.com>
2019-11-29 16:28:17 -0300

    product source

---
+++ product-stream/src/main/java/product/stream/Application.java
@@ -0,0 +1,10 @@
+package product.stream;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
